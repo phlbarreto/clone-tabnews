d76a65544ec2e20c7c42cc7d438c04276f7ffc6b
Pedro Barreto <phlbarreto90@gmail.com>
2025-10-10 16:45:08 -0300

    adiciona home page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  const title = `
+  Anne, obrigado por sempre acreditar em mim e jogar minha autoestima la no alto, te amo ❤
+  `;
+
+  return <h1>{title}</h1>;
+}
+
+export default Home;
